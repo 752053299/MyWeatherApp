8e9f3c61b82b6fca51a59800d758b4189dd02b2d
752053299 <752053299@qq.com>
2017-12-10 12:32:07 +0800

    创建app导入litePal库

---
+++ app/src/main/java/com/example/guiwu/myweather/db/Provience.java
@@ -0,0 +1,11 @@
+package com.example.guiwu.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 乔冠 on 2017/12/10. 省的javaBean
+ */
+
+public class Provience extends DataSupport {
+
+}
